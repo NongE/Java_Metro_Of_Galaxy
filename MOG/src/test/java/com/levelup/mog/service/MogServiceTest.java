c1b7ea82963901d55dc7976762efafacc1d2aedb
NongE <45160294+NongE@users.noreply.github.com>
2021-06-19 21:25:46 +0900

    getStationNamesTest 테스트 코드 변경 및 getAllStationLinesTest 테스트 코드 추가
    - 저장된 역사가 많아짐에 따라 가져온 역사의 개수를 확인하는 방식으로 getStationNamesTest 의 테스트 코드를 변경
    - 특정 역사의 호선을 모두 가져오는 getAllStationLinesTest 테스트 추가

--- MOG/src/test/java/com/levelup/mog/service/MogServiceTest.java
+++ MOG/src/test/java/com/levelup/mog/service/MogServiceTest.java
@@ -1,11 +1,13 @@
 package com.levelup.mog.service;
 
+import com.levelup.mog.database.SubwayId;
 import com.levelup.mog.repository.MogRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -17,40 +19,56 @@ class MogServiceTest {
     @Autowired
     MogRepository mogRepository;
 
-
     @Test
-    void getStationNames() {
+    void getStationNamesTest() {
         //given
         List<String> stationNames = new ArrayList<>();
-
-        //when
         mogRepository.findAll().forEach(stationIndex -> {
                     stationNames.add(stationIndex.SubwayIdToDto().getStationName());
                 }
         );
+
+        //when
         TreeSet<String> deduplicationStationName = new TreeSet<>(stationNames);
 
         //then
-        List<String> compareList = new ArrayList<>() {
-            {
-                add("군자");
-                add("어린이대공원");
-                add("건대입구");
-                add("뚝섬 유원지");
-                add("청담");
-                add("강남구청");
-                add("학동");
-                add("논현");
-                add("반포");
-                add("고속버스터미널");
-            }
-
-        };
-
-        assertThat(deduplicationStationName.containsAll(compareList));
+        assertThat(deduplicationStationName.size() == 10);
     }
 
     @Test
-    void getStationInfo() {
+    void getAllStationLinesTest() {
+        //given
+        List<String> getLines1 = new ArrayList<>();
+        List<String> getLines2 = new ArrayList<>();
+        List<String> getLines3 = new ArrayList<>();
+
+        // have 2 lines over
+        List<SubwayId> exampleLines1 = mogRepository.findBySubwayIdEmbStationName("건대입구");
+        // have 1 line
+        List<SubwayId> exampleLines2 = mogRepository.findBySubwayIdEmbStationName("어린이대공원");
+        // not have
+        List<SubwayId> exampleLines3 = mogRepository.findBySubwayIdEmbStationName("Temp");
+
+        //when
+        exampleLines1.forEach(subwayId -> {
+            getLines1.add(subwayId.SubwayIdToDto().getLineNumber());
+        });
+        exampleLines2.forEach(subwayId -> {
+            getLines2.add(subwayId.SubwayIdToDto().getLineNumber());
+        });
+        exampleLines3.forEach(subwayId -> {
+            getLines3.add(subwayId.SubwayIdToDto().getLineNumber());
+        });
+
+        //then
+        assertThat(getLines1.containsAll(new ArrayList<String>(Arrays.asList("2호선", "7호선"))));
+        assertThat(getLines2.containsAll(new ArrayList<String>(Arrays.asList("7호선"))));
+        assertThat(getLines3.size() == 0);
+
+    }
+
+    @Test
+    void getStationInfo(){
+
     }
 }
