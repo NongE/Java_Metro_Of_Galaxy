ab83a4a8c3a2bb08a5f1aa1763fafae5d6d645aa
NongE <45160294+NongE@users.noreply.github.com>
2021-06-22 23:28:54 +0900

    SetProperty를 Config에 등록하여 관리하도록 수정
    - SetProperty의 기존 어노테이션 삭제 및 SpringConfig에 추가
    - 순환 참조를 막기 위해 private final 로 선언한 변수 setProperty 삭제 및 mogService에 함수(setProperty())로 주입함

--- MOG/src/main/java/com/levelup/mog/config/SetProperty.java
+++ MOG/src/main/java/com/levelup/mog/config/SetProperty.java
@@ -1,9 +1,7 @@
 package com.levelup.mog.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties("api")
 public class SetProperty {
 
--- MOG/src/main/java/com/levelup/mog/config/SpringConfig.java
+++ MOG/src/main/java/com/levelup/mog/config/SpringConfig.java
@@ -5,8 +5,10 @@ import com.levelup.mog.repository.PredictSubwayUserRepository;
 import com.levelup.mog.repository.SubwayIdRepository;
 import com.levelup.mog.repository.SubwayInfoRepository;
 import com.levelup.mog.service.MogService;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class SpringConfig {
@@ -14,22 +16,29 @@ public class SpringConfig {
     private final SubwayIdRepository subwayIdRepository;
     private final SubwayInfoRepository subwayInfoRepository;
     private final PredictSubwayUserRepository predictSubwayUserRepository;
-    private final SetProperty setProperty;
 
-    public SpringConfig(SubwayIdRepository subwayIdRepository, SubwayInfoRepository subwayInfoRepository, PredictSubwayUserRepository predictSubwayUserRepository, SetProperty setProperty) {
+    public SpringConfig(SubwayIdRepository subwayIdRepository,SubwayInfoRepository subwayInfoRepository, PredictSubwayUserRepository predictSubwayUserRepository) {
         this.subwayIdRepository = subwayIdRepository;
         this.subwayInfoRepository = subwayInfoRepository;
         this.predictSubwayUserRepository = predictSubwayUserRepository;
-        this.setProperty = setProperty;
     }
 
     @Bean
     public MogService mogService(){
-        return new MogService(subwayIdRepository, subwayInfoRepository, predictSubwayUserRepository, setProperty);
+        return new MogService(subwayIdRepository, subwayInfoRepository, predictSubwayUserRepository, setProperty());
     }
+
+    @ConfigurationProperties("api")
+    @Bean
+    public SetProperty setProperty(){
+        return new SetProperty();
+    }
+
 
     @Bean
     public TimeCalculation timeCalculation(){
         return new TimeCalculation();
     }
+
+
 }
