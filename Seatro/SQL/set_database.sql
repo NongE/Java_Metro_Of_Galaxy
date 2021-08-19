002e7ad4aae02baac90c78249abb97b88d2f37b9
NongE <45160294+NongE@users.noreply.github.com>
2021-08-19 23:36:04 +0900

    DB 구조 변경에 따른 SQL, Trigger 재작성
    - 공통: 진행방향 정보 추가
    - surround_stations 테이블 구조 변경, 역사 명을 의미하는 특정한 정수 값을 저장, 이후 join을 통해 역사 명 매칭
    - station_users 테이블에서 열차 시간을 pk로 설정하여 컬럼 수를 줄임
    - 이와 관련하여 Trigger 수정

--- Seatro/SQL/set_database.sql
+++ Seatro/SQL/set_database.sql
@@ -12,12 +12,26 @@ CREATE TABLE subway_stations(
 
 INSERT INTO subway_stations VALUES ("7호선", "건대입구");
 INSERT INTO subway_stations VALUES ("2호선", "건대입구");
-INSERT INTO subway_stations VALUES ("5호선", "군자");
+
+
+CREATE TABLE surround_stations(
+ line_number varchar(10),
+ station_name varchar(15),
+ direction int,
+ surround_station varchar(15),
+ PRIMARY KEY(line_number, station_name, direction)
+);
+
+INSERT INTO surround_stations VALUES ("7호선", "건대입구", 0, "어린이대공원");
+INSERT INTO surround_stations VALUES ("7호선", "건대입구", 1, "뚝섬 유원지");
+INSERT INTO surround_stations VALUES ("2호선", "건대입구", 0, "성수");
+INSERT INTO surround_stations VALUES ("2호선", "건대입구", 1, "구의");
 
 
 CREATE TABLE station_detail_users(
  line_number varchar(10),
  station_name varchar(15),
+ direction int,
  check_in_time int,
  cabin_1 int,
  cabin_2 int,
@@ -27,78 +41,128 @@ CREATE TABLE station_detail_users(
  cabin_6 int,
  cabin_7 int,
  cabin_8 int,
- PRIMARY KEY(line_number, station_name, check_in_time)
+ PRIMARY KEY(line_number, station_name, direction, check_in_time)
 );
 
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 3, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 10, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 2, 5, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 3, 3, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 4, 10, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 5, 5, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 6, 3, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 7, 10, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 8, 5, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 9, 3, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 10, 10, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 11, 5, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 12, 3, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 13, 10, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 14, 5, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 15, 10, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 16, 5, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 17, 3, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 18, 10, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 19, 5, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 20, 10, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 21, 5, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 22, 3, 10, 10, 10, 10, 10, 10, 10);
-INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 23, 3, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 0, 3, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 1, 10, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 2, 5, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 3, 3, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 4, 10, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 5, 5, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 6, 3, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 7, 10, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 8, 5, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 9, 3, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 11, 5, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 12, 3, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 13, 10, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 14, 5, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 15, 10, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 16, 5, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 17, 3, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 18, 10, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 19, 5, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 20, 10, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 21, 5, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 22, 3, 10, 10, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 0, 23, 3, 10, 10, 10, 10, 10, 10, 10);
+
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 0, 3, 30, 30, 30, 30, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 1, 3, 19, 19, 19, 19, 19, 19, 19);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 2, 5, 19, 11, 11, 11, 11, 19, 19);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 3, 3, 19, 19, 19, 19, 19, 19, 19);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 4, 3, 3, 3, 3, 3, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 5, 5, 3, 3, 3, 3, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 6, 3, 3, 50, 50, 50, 50, 50, 3);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 7, 3, 3, 3, 3, 3, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 8, 5, 3, 3, 3, 3, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 9, 3, 3, 3, 2, 2, 2, 2, 2);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 10, 3, 3, 3, 2, 2, 2, 2, 2);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 11, 5, 2, 2, 2, 2, 2, 2, 2);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 12, 3, 2, 2, 2, 2, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 13, 10, 10, 1, 1, 1, 1, 1, 1);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 14, 5, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 15, 1, 1, 11, 11, 11, 11, 11, 11);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 16, 5, 11, 11, 11, 11, 11, 11, 11);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 17, 3, 11, 11, 11, 11, 11, 11, 11);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 18, 11, 11, 11, 1, 1, 1, 1, 1);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 19, 5, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 20, 1, 1, 1, 1, 1, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 21, 5, 13, 13, 13, 13, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 22, 3, 13, 13, 13, 13, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("7호선", "건대입구", 1, 23, 3, 13, 13, 13, 13, 10, 10, 10);
+
+--
+
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 0, 3, 13, 13, 13, 13, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 1, 13, 13, 13, 13, 13, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 2, 5, 13, 13, 13, 13, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 3, 3, 13, 13, 13, 13, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 4, 13, 13, 13, 13, 13, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 5, 5, 14, 14, 14, 14, 14, 14, 14);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 6, 3, 14, 14, 14, 14, 14, 14, 14);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 7, 14, 14, 14, 14, 14, 14, 14, 14);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 8, 5, 14, 14, 14, 14, 14, 14, 14);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 9, 3, 14, 14, 14, 14, 14, 14, 14);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 10, 14, 14, 14, 14, 14, 14, 14, 14);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 11, 5, 14, 14, 14, 9, 9, 9, 9);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 12, 3, 9, 9, 9, 9, 9, 9, 9);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 13, 9, 9, 9, 9, 9, 9, 9, 9);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 14, 5, 9, 9, 9, 9, 9, 9, 9);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 15, 9, 9, 9, 9, 9, 9, 9, 9);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 16, 5, 9, 9, 9, 9, 9, 9, 9);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 17, 3, 9, 9, 10, 10, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 18, 10, 8, 8, 8, 8, 8, 8, 8);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 19, 5, 8, 8, 8, 8, 8, 8, 8);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 20, 8, 8, 8, 8, 8, 8, 8, 8);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 21, 5, 8, 8, 8, 8, 8, 8, 8);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 22, 3, 8, 8, 8, 8, 8, 8, 8);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 0, 23, 3, 8, 8, 8, 10, 10, 10, 10);
+
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 0, 3, 30, 30, 30, 30, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 1, 3, 15, 15, 15, 15, 19, 19, 19);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 2, 5, 19, 11, 11, 11, 11, 19, 19);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 3, 3, 19, 19, 11, 11, 11, 19, 19);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 4, 3, 3, 3, 3, 3, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 5, 5, 3, 3, 3, 3, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 6, 3, 3, 50, 45, 45, 45, 50, 3);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 7, 3, 3, 3, 3, 3, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 8, 5, 3, 3, 3, 3, 3, 3, 3);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 9, 3, 3, 3, 2, 2, 2, 2, 2);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 10, 2, 2, 2, 7, 7, 7, 7, 2);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 11, 5, 2, 2, 2, 2, 2, 2, 2);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 12, 3, 6, 6, 2, 2, 10, 10, 10);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 13, 10, 10, 1, 1, 1, 1, 1, 1);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 14, 5, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 15, 1, 1, 11, 13, 13, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 16, 5, 13, 13, 13, 13, 11, 11, 11);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 17, 3, 11, 11, 11, 11, 11, 11, 11);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 18, 11, 11, 11, 5, 5, 5, 5, 1);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 19, 5, 1, 9, 9, 9, 9, 9, 1);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 20, 1, 1, 1, 1, 1, 13, 13, 13);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 21, 5, 13, 16, 16, 16, 16, 16, 16);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 22, 3, 16, 16, 16, 16, 16, 16, 16);
+INSERT INTO station_detail_users VALUES ("2호선", "건대입구", 1, 23, 3, 16, 13, 13, 13, 10, 10, 10);
+
 
 CREATE TABLE station_users(
  line_number varchar(10),
  station_name varchar(15),
- time_0 int,
- time_1 int,
- time_2 int,
- time_3 int,
- time_4 int,
- time_5 int,
- time_6 int,
- time_7 int,
- time_8 int,
- time_9 int,
- time_10 int,
- time_11 int,
- time_12 int,
- time_13 int,
- time_14 int,
- time_15 int,
- time_16 int,
- time_17 int,
- time_18 int,
- time_19 int,
- time_20 int,
- time_21 int,
- time_22 int,
- time_23 int,
- PRIMARY KEY(line_number, station_name)
+ direction int,
+ check_in_time int,
+ people int,
+ PRIMARY KEY(line_number, station_name, direction, check_in_time)
 );
 
-
-CREATE TABLE surround_stations(
- line_number varchar(10),
- station_name varchar(15),
- right_station varchar(15),
- left_station varchar(15),
- PRIMARY KEY(line_number, station_name, right_station, left_station)
-);
-
-INSERT INTO surround_stations VALUES ("7호선", "건대입구", "어린이대공원", "뚝섬 유원지");
-INSERT INTO surround_stations VALUES ("2호선", "건대입구", "성수", "구의");
-INSERT INTO surround_stations VALUES ("5호선", "군자", "장한평", "아차산");
-
-
-
+select * from subway_stations;
 select * from station_detail_users;
 select * from station_users;
+select * from surround_stations;
+
+select * from station_detail_users as sdu left join surround_stations as ss on sdu.direction = ss.direction;
+
+select max(people) from station_users where check_in_time = 3;
+
+update station_detail_users set cabin_1 = 100 where cabin_1 = 3 and direction = 0 and check_in_time = 0 cabin_2 = 13;
